27b8efe28ec8b2765bc8c423f5adb83a926d4ab7
kl2200031113 <164400055+kl2200031113@users.noreply.github.com>
2024-12-07 11:07:07 +0530

    v1

---
+++ Controller.java
@@ -0,0 +1,22 @@
+package com.klef.jfsd.exam.controller;
+
+import com.klef.jfsd.exam.model.Order;
+import com.klef.jfsd.exam.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@Valid @RequestBody Order order) {
+        Order savedOrder = orderService.saveOrder(order);
+        return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+    }
+}
